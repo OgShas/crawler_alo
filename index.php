015be181d9576dce8eab6b80f09894fed343ef95
ogshas <shasog@gmail.com>
2023-11-20 11:18:05 +0200

    through menu

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ use Crwlr\Crawler\Steps\Loading\Http;
 use Crwlr\Crawler\Stores\SimpleCsvFileStore;
 
 
+/*
 (new MyCrawler())
     ->setStore(new SimpleCsvFileStore('./store', 'alo.bg'))
     ->input('https://www.alo.bg/obiavi/avto-moto/avtomobili-djipove-pikapi/')
@@ -37,47 +38,34 @@ use Crwlr\Crawler\Stores\SimpleCsvFileStore;
     )
     ->runAndTraverse();
 
-/*
+*/
+
 
 (new MyCrawler())
-    ->setStore(new SimpleCsvFileStore('./store','alo.bg'))
-    ->input('https://www.alo.bg/obiavi/avto-moto/avtomobili-djipove-pikapi/')
-    ->addStep(Http::get())
+    ->setStore(new SimpleCsvFileStore('./store-Custom', 'alo.bg'))
+    ->input('https://www.alo.bg/obiavi/uslugi/')
     ->addStep(
-        Html::root()
-        ->extract([
-            'title' => 'h3',
-            'info' =>'[id^="adrows_"]',
-        ])
-        ->addLaterToResult(['title'])
+        Http::get()
+           // ->paginate('.paginator_wrapper', 1)
     )
-    ->addStep(Http::get()->useInputKey('info'))
-    ->addStep(
-        Html::root()
+    ->addStep(Html::each('#categorymenu .main')
         ->extract([
-            'name' => 'h3',
-            'url' => 'a',
-            'price' => 'nowrap'
+            'title' => Dom::cssSelector('a')->first()->innerText(),
+            'url' => Dom::cssSelector('a')->first()->link()
         ])
-        ->addToResult()
+    )->input('url')
+    ->addStep(
+        Http::get()
+    )
+    ->addStep(
+        Html::each('[id^="adrows_"]')
+            ->extract([
+                'title' => Dom::cssSelector('h3')->first(),
+               // 'url' => Dom::cssSelector('a')->first()->link(),
+                //'price' => Dom::cssSelector('.nowrap')->first(),
+                // 'p' => Dom::cssSelector('p')->first()->innerText(),
+            ])
+            ->addToResult()
     )
     ->runAndTraverse();
 
-
-/*
-
-//Crawling Whole Website
-
-(new MyCrawler())
-    ->setStore(new SimpleCsvFileStore('./Store-WholeWeb', 'alo.bg'))
-    ->input('https://www.alo.bg')
-    ->addStep(
-        Http::crawl()
-            ->pathStartsWith('/obiavi/zapoznanstva-eskort/')
-            ->depth(2)
-
-        ->addToResult()
-    )
-->runAndTraverse();
-
-*/
